b87b050b743acbc29a7f6faf57fbc85f7018f086
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2019-02-25 21:10:50 +0100

    reuse resolve_scheme in _wrap_test.sh

--- industrial_ci/_wrap_test.sh
+++ industrial_ci/_wrap_test.sh
@@ -20,27 +20,16 @@
 set -e # exit script on errors
 
 if [ -n "$_EXTERNAL_REPO" ]; then
+    export TRAVIS_BUILD_DIR; TRAVIS_BUILD_DIR=$(mktemp -d)
+    source ./industrial_ci/src/workspace.sh
+    declare -a parts
+    parts=($(resolve_scheme "$_EXTERNAL_REPO")) # name, type, url, version
 
-    export TRAVIS_BUILD_DIR=$(mktemp -d)
+    git clone "${parts[2]}" "$TRAVIS_BUILD_DIR"
+    git -C "$TRAVIS_BUILD_DIR" checkout "${parts[3]}"
 
-    repo_name=${_EXTERNAL_REPO%%#*}
-    repo_branch=${_EXTERNAL_REPO##*#}
-
-    if [ "$repo_branch" = "$_EXTERNAL_REPO" ]; then # if branch name was not provided
-        repo_branch="HEAD"
-    fi
-
-    if [[ "$repo_name" =~ ^[a-zA-Z][\w+-\.]*: ]]; then # stars with scheme (RFC 3986)
-        repo_url=$repo_name
-    else
-        repo_url="https://github.com/$repo_name.git" # treat as github repo
-    fi
-
-    git clone "$repo_url" "$TRAVIS_BUILD_DIR"
-    git -C "$TRAVIS_BUILD_DIR" checkout "$repo_branch"
-
-    urlbasename=${repo_url##*/}
-    urldirname=${repo_url%/$urlbasename}
+    urlbasename=${parts[2]##*/}
+    urldirname=${parts[2]%/$urlbasename}
     export TRAVIS_REPO_SLUG="${urldirname##*/}/${urlbasename%.git}"
 fi
 
